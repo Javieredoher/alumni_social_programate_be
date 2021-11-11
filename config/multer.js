58d0515cd3ded33bc23e4aa72c2ce5e98d38cdaa
JhonMos <jhonmos14@hotmail.com>
2021-11-11 13:19:04 -0500

    11-11-21| validacion extension y tamaño de la imagen|JMV

--- config/multer.js
+++ config/multer.js
@@ -5,10 +5,21 @@ const storage = multer.diskStorage({
     cb(null, './src/upload/')
   },
   filename: function (req, file, cb) {
-    cb(null, `${file.fieldname}-${Date.now()}.png`)
+    cb(null, `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`)
   }
 })
 
-const upload = multer({ storage })
+const upload = multer({ 
+  storage,
+  limits: {fileSize: 200000},
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/;
+    const minetype = filetypes.test(file.mimetype.split('/')[1])
+    if(minetype){
+      return cb(null, true);
+    }
+    cb("Error: Archivo debe ser una imagen valida");
+  }
+})
 
 module.exports = upload
